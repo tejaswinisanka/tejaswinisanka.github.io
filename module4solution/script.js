582fa083e29d006829b29af620a686eecaacebf6
Sanka Tejaswini <tejaswinisanka@gmail.com>
2021-07-11 23:14:01 +0530

    Update script.js

--- module4solution/script.js
+++ module4solution/script.js
@@ -2,13 +2,13 @@ var names=new Array();
 names[0]="Yaakov";
 names[1]="John";
 names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
 
 
 for (var i = 0; i < names.length; i++) {
